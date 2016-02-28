7a9fdb9ed531a4e1c9c142b34c106c846836c9cb
Zero-One101 <stephenbadrick@gmail.com>
2016-02-28 02:04:48 +0000

    Intial project commit

---
+++ Source/FinalProject/FinalProjectGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "FinalProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFinalProjectGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AFinalProjectGameMode();
+};
+
+
+
